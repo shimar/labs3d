15ab3a5898be136f40c39c87bd0a9a456369cc59
shimar <shima01dev@gmail.com>
2014-04-27 14:35:31 +0900

    add timeline page.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'labs3d' });
 });
 
+/* GET timeline page. */
+router.get('/timeline', function(req, res) {
+  res.render('timeline', { title: 'timeline - labs3d' });
+});
+
 module.exports = router;
